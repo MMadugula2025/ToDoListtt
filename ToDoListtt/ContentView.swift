00b6d3b6c3ff4611ec60d4aaf474cefd0311308a
Mahati Madugula <mahati.madugula@icloud.com>
2023-06-13 10:31:33 -0500

    Initial Commit

---
+++ ToDoListtt/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ToDoListtt
+//
+//  Created by Mahati Madugula on 6/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToDoListtt/ToDoListttApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListttApp.swift
+//  ToDoListtt
+//
+//  Created by Mahati Madugula on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListttApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
